4b51f14138db8f810f5a42765b4c270d39d6fd0f
Hossam Albasuony <albasuonyh@gmail.com>
2026-04-09 15:36:07 +0200

    update project

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-import serverless from 'serverless-http';
-import app from '../src/app.js';
-import { connectDB } from '../src/config/db.js';
+import serverless from "serverless-http";
+import app from "../src/app.js";
+import { connectDB } from "../src/config/db.js";
 
 let isConnected = false;
 
@@ -9,17 +9,14 @@ const handler = serverless(app);
 export default async (req, res) => {
   try {
     if (!isConnected) {
-      const uri = process.env.MONGODB_URI;
-      if (!uri) throw new Error("MONGODB_URI missing");
-
-      await connectDB(uri);
+      await connectDB(process.env.MONGODB_URI);
       isConnected = true;
       console.log("Mongo Connected ✅");
     }
 
     return handler(req, res);
   } catch (err) {
-    console.error(err);
+    console.error("ERROR:", err);
     return res.status(500).json({ error: "Server error" });
   }
 };
